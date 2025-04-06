413bd1d1380561eff9489e2336164f4fd6297c22
liuwenjie <205920584@qq.com>
2025-04-06 12:30:35 +0800

    init

--- auth-service/src/main/java/com/iss/auth/controller/LoginController.java
+++ auth-service/src/main/java/com/iss/auth/controller/LoginController.java
@@ -1,7 +1,6 @@
-package com.iss.login.service.controller;
+package com.iss.auth.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
