64e669e5f90542f2259a856c47aa7f4a4fd230db
xiurui2002 <xiuruiyou@gmail.com>
2025-04-25 02:21:49 +0800

    gateway and a demo

--- clinic-service/src/main/java/com/iss/clinic/controller/ClinicController.java
+++ clinic-service/src/main/java/com/iss/clinic/controller/ClinicController.java
@@ -1,14 +1,29 @@
 package com.iss.clinic.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import com.iss.clinic.feignService.LoginFeignService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/clinic")
 public class ClinicController {
-    @GetMapping("/status")
-    public String getClinicStatus() {
-        return "Clinic service is running!";
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Autowired
+    LoginFeignService loginFeignService;
+
+    @RequestMapping("/add")
+    public String add(){
+        System.out.println("下单成功！");
+//        String msg = restTemplate.getForObject("http://login-service/login", String.class);
+        String msg = loginFeignService.reduct();
+        return "Hello World +"+msg;
     }
+//    @GetMapping("/status")
+//    public String getClinicStatus() {
+//        return "Clinic service is running!";
+//    }
 }
