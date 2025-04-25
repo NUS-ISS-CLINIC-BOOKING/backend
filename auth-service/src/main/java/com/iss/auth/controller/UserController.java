45a009f8928044b446e373a64e087eb246b004d6
xiurui2002 <xiuruiyou@gmail.com>
2025-04-25 15:10:19 +0800

    mybatis-test

--- auth-service/src/main/java/com/iss/auth/controller/UserController.java
+++ auth-service/src/main/java/com/iss/auth/controller/UserController.java
@@ -2,22 +2,24 @@
 //
 //import com.iss.auth.entity.User;
 //import com.iss.auth.mapper.UserMapper;
+//import com.iss.auth.service.UserService;
+//import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RestController;
 //
 //import java.util.List;
 //
 //@RestController
+//@RequestMapping("/user")
 //public class UserController {
 //
-//    private final UserMapper userMapper;
+//    @Autowired
+//    private UserService userService;
 //
-//    public UserController(UserMapper userMapper) {
-//        this.userMapper = userMapper;
-//    }
-//
-//    @GetMapping("/users")
-//    public List<User> allUsers() {
-//        return userMapper.selectAll();
+//    @GetMapping("/list")
+//    public List<User> list() {
+//        return userService.getAllUsers();
 //    }
 //}
+//
