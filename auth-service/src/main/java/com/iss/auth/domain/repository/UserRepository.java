fee2798d8917c480e1a6add3589556eee9721b07
liuwenjie.049 <liuwenjie.049@bytedance.com>
2025-05-22 23:13:20 +0800

    修改端口
    Change-Id: Iae961842f4cd2e0e3a4b310b84ce2ee773c9ed33

--- auth-service/src/main/java/com/iss/auth/domain/repository/UserRepository.java
+++ auth-service/src/main/java/com/iss/auth/domain/repository/UserRepository.java
@@ -5,4 +5,5 @@ import com.iss.auth.domain.entity.User;
 public interface UserRepository {
     User findByEmail(String email);
     void save(User user);
+    void ModifyUserHealthInfo(long userId, String healthInfo);
 }
