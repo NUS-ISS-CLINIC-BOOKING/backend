e19ada380fa4ca5953888a160a4bfa1be67bd8f6
liuwenjie.049 <liuwenjie.049@bytedance.com>
2025-05-22 20:08:13 +0800

    修改端口
    Change-Id: Ie9264c56dd14de06b6b0c2534228a670f6b8f705
    
    # Conflicts:
    #	gateway/src/main/resources/application.yml

--- patient-medicine-service/src/main/java/com/iss/patient_medicine/PatientMedicineApplication.java
+++ patient-medicine-service/src/main/java/com/iss/patient_medicine/PatientMedicineApplication.java
@@ -3,9 +3,11 @@ package com.iss.patient_medicine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;  // 启用 Nacos 服务发现
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient  // 必须添加此注解以注册到 Nacos
+@EnableFeignClients
 public class PatientMedicineApplication {
     public static void main(String[] args) {
         SpringApplication.run(PatientMedicineApplication.class, args);
--- patient-medicine-service/src/main/java/com/iss/patient_medicine/controller/PatientMedicineController.java
+++ patient-medicine-service/src/main/java/com/iss/patient_medicine/controller/PatientMedicineController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/patient_medicine")
+@RequestMapping("/api/patient_medicine")
 public class PatientMedicineController {
     @GetMapping("/status")
     public String getPatientMedicineStatus() {
