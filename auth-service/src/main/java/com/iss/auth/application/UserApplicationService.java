5ccb8758d31cf8c479e036b1e3b080b67a2419bd
liuwenjie.049 <liuwenjie.049@bytedance.com>
2025-05-22 23:13:20 +0800

    修改端口
    Change-Id: Iae961842f4cd2e0e3a4b310b84ce2ee773c9ed33

--- auth-service/src/main/java/com/iss/auth/application/UserApplicationService.java
+++ auth-service/src/main/java/com/iss/auth/application/UserApplicationService.java
@@ -1,5 +1,7 @@
 package com.iss.auth.application;
 
+import com.iss.auth.domain.entity.User;
+import com.iss.auth.domain.repository.UserRepository;
 import com.iss.auth.dto.ModifyHealthInfoCommand;
 import com.iss.auth.dto.ModifyHealthInfoResult;
 import jakarta.validation.Valid;
@@ -9,7 +11,11 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserApplicationService {
-    public ModifyHealthInfoResult modifyHealthInfo(Long UserID, @Valid ModifyHealthInfoCommand request) {
+
+    private final UserRepository userRepository;
+
+    public ModifyHealthInfoResult modifyHealthInfo(long UserID, @Valid ModifyHealthInfoCommand request) {
+        userRepository.ModifyUserHealthInfo(UserID, request.getAllergyInfo());
         return null;
     }
 }
